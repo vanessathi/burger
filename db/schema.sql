b5eb4e8dce2bbeae964fa3b51bcb41e0a99b896a
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-04 19:19:40 -0600

    ran schema and seed file through command line and added config folder

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,13 @@
-DROP DATABASE IF EXISTS burgersdb;
+DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE burgersdb;
-USE burgersdb;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers
 (
     id int NOT NULL
     AUTO_INCREMENT,
-	burger_name varchar
-    (255) NOT NULL,
+	burger_name varchar (255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY
     (id)
