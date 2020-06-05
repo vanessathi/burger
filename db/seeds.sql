32d6ff4950c5f207d4e9abb8708837dc1bbc5160
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-04 19:11:27 -0600

    added different types of burgers in seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
-INSERT INTO burgers (burger_name)
-VALUES ('Royal Egg Burger');
+INSERT INTO burgers (burger_name) VALUES ('Blue is the Warmest Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Four Cheddar Bout It Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chorizo Your Own Adventure Burger');
